24201fdc063ba95144e30af9e22753d2d257e7cd
ReniPY <deserts876@gmail.com>
2024-12-05 21:00:28 +0700

    Add files via upload

---
+++ prodject5.py
@@ -0,0 +1,81 @@
+import file_operations
+from faker import Faker
+import random
+import os 
+ 
+
+def main():
+    fake = Faker("ru_RU")
+    skills = [
+    "Стремительный прыжок",
+    "Электрический выстрел",
+    "Ледяной удар",
+    "Стремительный удар",
+    "Кислотный взгляд",
+    "Тайный побег",
+    "Ледяной выстрел",
+    "Огненный заряд",
+    ]
+
+
+    alphabet = {
+ 'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+ 'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+ 'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+ 'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+ 'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+ 'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+ 'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+ 'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+ 'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+ 'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+ 'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+ 'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+ 'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+ 'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+ 'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+ 'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+ 'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+ 'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+ 'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+ 'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+ 'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+ 'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+    }
+
+
+    for index in range(len(skills)):
+        for key in alphabet.keys():
+            skills[index] = skills[index].replace(key, alphabet[key])
+
+
+    if not os.path.isdir("NPC_Cards"):
+        os.mkdir("NPC_Cards") 
+
+
+    for i in range(10):
+      i = i + 1
+      skills = random.sample(skills, 8)
+      context = {
+      "first_name": fake.first_name_male(),
+      "last_name": fake.last_name_male(),
+      "job": fake.job(),
+      "town": fake.city(),
+      "strength": random.randint(3, 18),
+      "agility": random.randint(3, 18),
+      "endurance": random.randint(3, 18),
+      "intelligence": random.randint(3, 18),
+      "luck": random.randint(3, 18),
+      "skill_1" : skills[0],
+      "skill_2" : skills[1],
+      "skill_3" : skills[2],
+      }
+
+
+      card_name = f"NPC_card_{i}.svg"
+      file_operations.render_template("charsheet.svg", f"NPC_Cards/{card_name}", context)
+
+
+if __name__ == "__main__":
+    main()
